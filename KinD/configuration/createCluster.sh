22963f124b51d4bc6393a8a507f18472d2edd0b8
Hernan J. Larrea <hjlarrea@hotmail.com>
2020-05-25 15:58:01 -0300

    Update kind configs.

--- KinD/configuration/createCluster.sh
+++ KinD/configuration/createCluster.sh
@@ -1,8 +1,8 @@
-kind create cluster --name=cluster01 --config=./config.yaml
+kind create cluster --name=cluster01 --config=./kindCalicoIngress.yaml
 kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
-sleep 30
-kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
-sleep 30
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 sleep 60
-kubectl apply -f ./test.yaml
+kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
+sleep 60
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+sleep 90
+kubectl apply -f ./ingressDemoApp.yaml
