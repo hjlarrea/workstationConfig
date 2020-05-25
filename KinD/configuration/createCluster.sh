e1e0b51e8df69dce7d6a020bc688a6c6054548b7
Hernan J. Larrea <hjlarrea@hotmail.com>
2020-05-25 16:36:07 -0300

    Update KinD config files.

--- KinD/configuration/createCluster.sh
+++ KinD/configuration/createCluster.sh
@@ -1,8 +1,25 @@
-kind create cluster --name=cluster01 --config=./kindCalicoIngress.yaml
-kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+#!/bin/bash
+
+config="./kindCalicoIngress.yaml"
+
+# Spin up the cluster
+kind create cluster --name=cluster01 --config=$config
+
+# Install Calico CNI
+echo "Installing Calico."
+kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml > /dev/null
+echo "Sleeping for 60 seconds..."
 sleep 60
+echo "Updating Calico environment varaibles."
 kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
+echo "Sleeping for 60 seconds..."
 sleep 60
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+
+# Install the KinD NGINX ingress controller
+echo "Installing KinD NGINX ingress controller."
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml > /dev/null
+echo "Sleeping for 90 seconds..."
 sleep 90
+
+# Deploy the demo app
 kubectl apply -f ./ingressDemoApp.yaml
